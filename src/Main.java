30d50b4a939bfafdc04199227801d70be72ee967
unknown <brucristian>
2026-04-23 18:59:25 -0500

    Cree menu interactivo

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.*;
+
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Biblioteca miBiblioteca = new Biblioteca();
+        Scanner leer = new Scanner(System.in);
+        int opcion = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            try {
+                System.out.println("\n--- MENU BIBLIOTECA UNIVERSIDAD DE CARTAGENA ---");
+                System.out.println("1. Registrar Autor");
+                System.out.println("2. Registrar Categoría");
+                System.out.println("3. Registrar Libro");
+                System.out.println("4. Realizar Préstamo");
+                System.out.println("5. Mostrar Estado del Sistema");
+                System.out.println("6. Salir");
+                System.out.print("Seleccione una opción: ");
+
+                opcion = leer.nextInt();
+                leer.nextLine();
+
+            } catch (InputMismatchException e) {
+                System.out.println("ERROR: Debe ingresar un valor numerico válido.");
+                leer.nextLine();
+                opcion = 0;
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error inesperado: " + e.getMessage());
+                leer.nextLine();
+            }
+
+        } while (opcion != 6);
     }
 }
