4ea7fc4088d80d4fb70c3cda5766130d971ed545
joan566 <joansebastianvergaravalencia@gmail.com>
2026-04-23 23:11:00 -0500

    Registrar Autor

--- src/controller/AutorController.java
+++ src/controller/AutorController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import model.Autor;
+import model.Biblioteca;
+
 public class AutorController {
+
+    public void AgregarAutor(int id, String nombre, String pais, int anioNacimmiento, Biblioteca biblioteca) {
+        Autor autor = new Autor(id, nombre, pais, anioNacimmiento);
+        biblioteca.registrarAutor(autor);
+    }
+
+    public void verDetallesAutor(Biblioteca biblioteca, Autor autor) {
+
+    }
 }
